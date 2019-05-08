a5b83c5ddc211f24d02b6a93e31d8a70deb48c0d
pyigyli <pyry.vahakangas@luukku.com>
2019-05-08 18:37:38 +0300

    osan 1 osa tehtävistä suoritettu

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+	return (
+		<h1>
+			{props.course}
+		</h1>
+	)
+}
+
+const Part = (props) => {
+	return (
+		<p>
+			{props.name} {props.exercises}
+		</p>
+	)
+}
+
+const Content = (props) => {
+	return (
+		<div>
+			{props.parts.map(part =>
+				<Part
+					name={part.name}
+					exercises={part.exercises}
+				/>
+			)}
+		</div>
+	)
+}
+
+const Total = (props) => {
+	let amount = 0;
+	props.parts.forEach(part => amount += part.exercises);
+
+	return (
+		<p>
+			yhteensä {amount} tehtävää
+		</p>
+	)
+}
+
+const App = () => {
+	const course = {
+		name: 'Half Stack -sovelluskehitys',
+		parts: [
+			{
+				name: 'Reactin perusteet',
+				exercises: 10
+			},
+			{
+				name: 'Tiedonvälitys propseilla',
+				exercises: 7
+			},
+			{
+				name: 'Komponenttien tila',
+				exercises: 14
+			}
+		]
+	}
+
+	return (
+		<div>
+			<Header course={course.name}/>
+			<Content parts={course.parts}/>
+			<Total parts={course.parts}/>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
