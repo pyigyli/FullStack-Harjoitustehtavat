5b938a2fbcc4bf9bf158be784bd79535bece81d4
pyigyli <pyry.vahakangas@luukku.com>
2019-05-10 01:49:32 +0300

    commit ennen herokuun puskemista

---
+++ osa3/puhelinluettelo/index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const morgan = require('morgan')
+morgan.token('person', function (req, res) {return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
+const cors = require('cors')
+app.use(cors())
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '045-1236543'
+  },
+  {
+    id: 2,
+    name: 'Arto Järvinen',
+    number: '041-21423123'
+  },
+  {
+    id: 3,
+    name: 'Lea Kutvonen',
+    number: '040-4323234'
+  },
+  {
+    id: 4,
+    name: 'Martti Tienari',
+    number: '09-784232'
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+  const id = Math.floor(Math.random() * 10000)
+  const person = req.body
+  if (!person || !person.name || !person.number) {
+    return res.status(400).json({error: 'both name and number are required'})
+  }
+  if (persons.filter(person => person.id === id).length > 0) {
+    return res.status(400).json({error: 'name must be unique'})
+  }
+  person.id = id
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id/delete', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end();
+});
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Puhelinluettelossa ${persons.length} henkilön tiedot</p>
+    <p>${new Date()}</p>
+  `)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
